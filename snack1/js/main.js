51334db16e42a46c6fa5b518d76bf3780330f3b7
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-06 14:42:53 +0200

    added base structure

---
+++ snack1/js/main.js
@@ -0,0 +1,7 @@
+/*
+Consegna esercizio:
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/

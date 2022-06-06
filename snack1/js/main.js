edf0b8b1a626b27359b3cb860c044f33bb0e7988
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-06 14:56:59 +0200

    add array snack 1

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -5,3 +5,29 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 */
+
+//I'll go to create an array of elements
+const containerBikes = [
+{
+    nome: "triban",
+    peso: "1.7",
+},
+{
+    nome: "van rysel",
+    peso: "1.4",
+},
+{
+    nome: "3T",
+    peso: "9",
+},
+{
+    nome: "protek bikes gravel",
+    peso: "2",
+},
+{
+    nome: "btwin",
+    peso: "10",
+},
+];
+
+

75d91a1794a1b9bd0bf2254c9e007e93eb1b15c7
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-06 15:42:42 +0200

    snack1 ended

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -6,15 +6,20 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 */
 
+/*symbols 
+> <
+``
+*/
+
 //I'll go to create an array of elements
 const containerBikes = [
 {
     nome: "triban",
-    peso: "1.7",
+    peso: "4",
 },
 {
     nome: "van rysel",
-    peso: "1.4",
+    peso: "3",
 },
 {
     nome: "3T",
@@ -26,8 +31,40 @@ const containerBikes = [
 },
 {
     nome: "btwin",
-    peso: "10",
+    peso: "5",
 },
 ];
+
+const containerLightBike = document.querySelector(".containerBikes");
+
+//I use the destructuring property to extrapolate the keys I need to read
+let {nome, peso} = containerBikes[3];
+console.table(containerBikes);
+
+let lightBike={
+    nome,
+    peso,
+};
+//for loop
+for(let i = 1 ; i<containerBikes.length; i++){
+    //object with keys name and weight
+    const bici = containerBikes[i];
+
+    //condition for my lightBike
+    if(lightBike.peso > bici.peso){
+        lightBike=bici
+    };
+
+    const template = `<div class="col 12">
+                        <h1>${nome}</h1>
+                        <h2>${peso}</h2>                 
+                      </div>
+                     `
+
+    containerLightBike.innerHTML = template; 
+}
+//I print to see if the result is correct
+console.log(lightBike)
+
 
 

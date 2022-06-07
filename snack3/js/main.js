f0e7a12a2b1935d4d81502c6f78bb08a0409ba9f
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-07 15:09:54 +0200

    added base structur

---
+++ snack3/js/main.js
@@ -0,0 +1,8 @@
+/*
+Snack 3
+Dato l’array di nomi:
+const myArray = [‘Michele’, ‘Fabio’, ‘Roberto’, ‘Giovanni’, ‘Simone’, ‘Chiara’];
+e dati due numeri min e max (min più piccolo di max).
+Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+Eseguiamo questo esercizio prima con forEach e poi con filter.
+*/
